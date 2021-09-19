990c1156bb0511f85dfb5551bb91bcb28dda6a32
Wil <wil9517@hotmail.com>
2021-09-19 12:18:17 -0500

    initial commit

---
+++ database/migrations/2021_09_19_082809_create_empleados_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('primer_nombre');
+            $table->string('apellido');
+            $table->bigInteger('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('empresas'); 
+            $table->string('correo');
+            $table->string('telefono');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
